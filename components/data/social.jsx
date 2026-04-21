aca63c935f60d939d9c2b13b200cc149e28f19d6
Federica Del Campo <federicadelcampo@Federicas-MacBook-Pro---024.local>
2026-04-21 10:10:40 -0300

    links updated

--- components/data/social.jsx
+++ components/data/social.jsx
@@ -5,10 +5,8 @@ const Social = () => {
     return (
         <>
             <ul>
-                <li><Link href="https://www.facebook.com" target="_blank"><i className="fab fa-facebook-f"></i></Link></li>
-                <li><Link href="https://twitter.com" target="_blank"><i className="fa-brands fa-x-twitter"></i></Link></li>
-                <li><Link href="https://www.behance.net" target="_blank"><i className="fab fa-behance"></i></Link></li>
-                <li><Link href="https://www.linkedin.com" target="_blank"><i className="fab fa-linkedin-in"></i></Link></li>
+                <li><Link href="https://www.instagram.com/sunergys.uy?igsh=cHYxemh2Nm5sNzg1&utm_source=qr" target="_blank"><i className="fab fa-instagram"></i></Link></li>
+                <li><Link href="https://www.linkedin.com/company/sunergys/" target="_blank"><i className="fab fa-linkedin-in"></i></Link></li>
             </ul>            
         </>
     );
